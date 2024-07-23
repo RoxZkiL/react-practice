378a910e12c96125f320f10fe28660e86a0fbda5
RoxZkiL <mttrox@gmail.com>
2024-07-23 16:13:44 -0400

    logic optimized

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import "./App.css";
 import TwitterCard from "./Components/TwitterCard";
 
 function App() {
+  const personas = [
+    {
+      name: "Jesus Matute",
+      userName: "RoxZkiL",
+    },
+    { name: "Miguel Angel Duran", userName: "midudev" },
+    {
+      name: "Cristiano Ronaldo",
+      userName: "cristiano",
+    },
+  ];
+
   return (
     <section className="App">
-      <TwitterCard name="Jesús Matute" userName="RoxZkiL" />
-      <TwitterCard name="Miguel Ángel Duran" userName="midudev" />
-      <TwitterCard name="Elon Musk" userName="elonmusk" />
+      {personas.map((persona, index) => (
+        <TwitterCard
+          key={index}
+          name={persona.name}
+          userName={persona.userName}
+        />
+      ))}
     </section>
   );
 }
